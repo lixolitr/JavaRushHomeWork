9575f8ee96f97a985e4a4abd524fea5a6ef98bc7
Dmytro Lykholit <lixolitr@gmail.com>
2017-01-03 21:37:50 +0200

    add multiple table

--- Solution.java
+++ Solution.java
@@ -31,6 +31,18 @@ public class Solution
 
         System.out.print(name + " захватит мир через " + age + " лет. Му-ха-ха!");
 
+        // multiples table
+        int sum =0;
+        for (int i =1; i<=10; i++){
+            for (int j = 1; j <= 10; j++){
+                sum = i*j;
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+
+        }
+
+
 
     }
 }
