b0e11ffc2a93326e458ca9ad53f9fa8d5d34eae7
Dmytro Lykholit <lixolitr@gmail.com>
2017-01-03 21:30:04 +0200

    input new options1

--- Solution.java
+++ Solution.java
@@ -6,7 +6,8 @@ name1 + name2 + name3 = Чистая любовь, да-да!
 Пример: Вася + Ева + Анжелика = Чистая любовь, да-да!
 */
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution
 {
@@ -15,11 +16,20 @@ public class Solution
         //напишите тут ваш код
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-
+        /*
         String name1 = reader.readLine();
         String name2 = reader.readLine();
         String name3 = reader.readLine();
 
+        */
+
+        System.out.println("Input age:");
+        int age = Integer.parseInt(reader.readLine());
+
+        System.out.println("Input name:");
+        String name = reader.readLine();
+
+        System.out.print(name + " захватит мир через " + age + " лет. Му-ха-ха!");
 
 
     }
